0979c210db1f08b92a36750867ca0119afa66181
martiniano <martiniano.leguizamon@bluealba.com>
2023-12-23 07:40:23 -0300

    Add Project Side Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import ProjectSidebar from "./components/ProjectSideBar";
+
 function App() {
+  const [projects, setProjects] = useState([]);
+  const [displayAddProjectForm, setDisplayAddProjectForm] = useState(false);
+
+  const handleAddProject = () => {
+    console.log("Im clicking Add Project");
+    setDisplayAddProjectForm(true);
+  };
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar onClick={handleAddProject} projects={projects} />
+    </main>
   );
 }
 
